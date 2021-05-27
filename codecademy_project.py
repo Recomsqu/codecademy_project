e7bc3a729112d715c94701e933772eb48fa82d27
Recomsqu <geusic@hotmail.com>
2021-05-27 13:06:52 +0200

    cards

---
+++ codecademy_project.py
@@ -0,0 +1,65 @@
+# BLACKJACK
+
+cards = [
+        { #HEARTS
+        "ace of hearts":[1,11], 
+        "two of hearts":2, 
+        "three of hearts":3, 
+        "four of hearts":4, 
+        "five of hearts":5, 
+        "six of hearts":6, 
+        "seven of hearts":7, 
+        "eight of hearts":8, 
+        "nine of hearts":9,
+        "ten of hearts":10, 
+        "queen of hearts":10, 
+        "jack of hearts":10, 
+        "king of hearts": 10, 
+        },
+        { #SPADES
+        "ace of spades": [1,11],
+        "two of spades":2,
+        "three of spades":3, 
+        "four of spades":4, 
+        "five of spades":5, 
+        "six of spades":6, 
+        "seven of spades":7, 
+        "eight of spades":8, 
+        "nine of spades":9, 
+        "ten of spades":10, 
+        "queen of spades":10, 
+        "jack of spades":10, 
+        "king of spades": 10
+        },
+        { #DIAMONDS
+        "ace of diamons": [1,11], 
+        "two of diamons":2, 
+        "three of diamons":3, 
+        "four of diamons":4, 
+        "five of diamons":5, 
+        "six of diamons":6, 
+        "seven of diamons":7, 
+        "eight of diamons":8, 
+        "nine of diamons":9, 
+        "ten of diamons":10, 
+        "queen of diamons":10, 
+        "jack of diamons":10, 
+        "king of diamons": 10
+        },
+        { #CLUBS
+        "ace of clubs": [1,11], 
+        "two of clubs":2,
+        "three of clubs":3, 
+        "four of clubs":4, 
+        "five of clubs":5, 
+        "six of clubs":6, 
+        "seven of clubs":7, 
+        "eight of clubs":8, 
+        "nine of clubs":9, 
+        "ten of clubs":10, 
+        "queen of clubs":10, 
+        "jack of clubs":10, 
+        "king of clubs": 10
+        }
+        ]
+
